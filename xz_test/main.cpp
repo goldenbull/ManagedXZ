6a46e5518a82105c15b1bac487c4f9cc25f0adf8
goldenbull <goldenbull@gmail.com>
2016-01-28 22:06:44 +0800

    copy include files and .dll files from xz-5.2.1-windows, add a c++ project to test

---
+++ xz_test/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include "lzma.h"
+
+#pragma comment(lib, "../api/lib/liblzma.lib")
+
+int main()
+{
+    lzma_stream stream;
+
+    return 0;
+}
